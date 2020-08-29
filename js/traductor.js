6c3b73d3c19a84adf910c5cce420b38da7d23694
Lisandro Scarrone <lisandro.sebsca@gmail.com>
2020-08-29 19:57:26 -0300

    Agrega traductor base

---
+++ js/traductor.js
@@ -0,0 +1,39 @@
+const $output = document.querySelector('#output');
+const $button = document.querySelector('#button');
+
+function getInput() {
+    const $input = document.querySelector('#input').value;
+    separarEnSilabas($input);
+}
+
+function separarEnSilabas(palabra) {
+    let palabraEnSilabas = silabaJS.getSilabas(palabra)['silabas'];
+    traductor(palabraEnSilabas);
+}
+
+function traductor(palabraEnSilabas) {
+    let traduccion = '';
+
+    palabraEnSilabas.forEach((objeto) => {
+        stringSilaba = objeto['silaba'].trim();
+        traduccion += stringSilaba;
+        if (stringSilaba.includes('a')) {
+            traduccion += 'pa ';
+        } else if (stringSilaba.includes('e')) {
+            traduccion += 'pe ';
+        } else if (stringSilaba.includes('i')) {
+            traduccion += 'pi ';
+        } else if (stringSilaba.includes('o')) {
+            traduccion += 'po ';
+        } else if (stringSilaba.includes('u')) {
+            traduccion += 'pu ';
+        } else if ((stringSilaba = 'y')) {
+            traduccion += 'pi ';
+        }
+    });
+    console.log(traduccion);
+}
+
+$button.onclick = function () {
+    getInput();
+};
