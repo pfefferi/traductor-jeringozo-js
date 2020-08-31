4d0fb74dae9ad5b4975f15933ebe28c94c407b30
Lisandro Scarrone <lisandro.sebsca@gmail.com>
2020-08-31 11:47:20 -0300

    Agrega verificación de errores
    Agrega la verificación del input y si hay errores los muestra en el div 'error'.

--- js/traductor.js
+++ js/traductor.js
@@ -1,13 +1,13 @@
-const $button = document.querySelector('#button');
+/////////////////// Traductor /////////////////////////////////////////////////////////////////
 
 function getInput() {
     const $input = document.querySelector('#input').value;
-    separarEnSilabas($input);
+    return $input;
 }
 
 function separarEnSilabas(palabra) {
     let palabraEnSilabas = silabaJS.getSilabas(palabra)['silabas'];
-    traductor(palabraEnSilabas);
+    return palabraEnSilabas;
 }
 
 function traductor(palabraEnSilabas) {
@@ -30,7 +30,7 @@ function traductor(palabraEnSilabas) {
             traduccion += 'pi ';
         }
     });
-    giveOutput(traduccion);
+    return traduccion;
 }
 
 function giveOutput(output) {
@@ -38,6 +38,63 @@ function giveOutput(output) {
     $output.value = output;
 }
 
+/////////////////// Verificacion del input ///////////////////////////////////////////////////////
+
+function errores(hayErrores) {
+    if (hayErrores) {
+        mostrarErrores(hayErrores);
+    }
+    if (!hayErrores) {
+        addClass();
+        giveOutput(traductor(separarEnSilabas(getInput())));
+    }
+}
+
+function validarInput(input) {
+    if (input.trim().length === 0) {
+        return 'El texto a traducir debe contener al menos un caracter.';
+    }
+    if (!/^[a-z]+$/i.test(input)) {
+        return 'El texto debe contener unicamente letras.';
+    } else {
+        return '';
+    }
+}
+
+////////////////// Exposicion de errores encontrados ////////////////////////////////////////////
+
+function mostrarErrores(errores) {
+    removeClass();
+    removeError();
+    addError(errores);
+}
+
+function removeClass() {
+    let divError = document.querySelector('#error');
+    divError.classList.remove('d-none');
+}
+
+function addClass() {
+    let divError = document.querySelector('#error');
+    divError.classList.add('d-none');
+}
+
+function removeError(error) {
+    let divError = document.querySelector('#error');
+    divError.textContent = '';
+}
+
+function addError(error) {
+    let divError = document.querySelector('#error');
+    divError.textContent = error;
+}
+
+/////////////////// Funcion onclick ///////////////////////////////////////////////////////////////
+
+const $button = document.querySelector('#button');
+
 $button.onclick = function () {
-    getInput();
+    errores(validarInput(getInput()));
 };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
